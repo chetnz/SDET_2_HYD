fe825ad27ac17c9525bd3f1ff10acf2ff38bfbf1
chetnz <nchetna@gmail.com>
2021-11-12 16:34:53 +0530

    thi si snew commit

---
+++ src/test/java/TestNg/practice/Chetna.java
@@ -0,0 +1,12 @@
+package TestNg.practice;
+
+public class Chetna {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is CHetna WOrkspace");
+
+	}
+
+}
